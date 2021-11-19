d7b524250c1f07f55dd8e9dd5438b18d2c772c39
Scherer <vicentin.py@gmail.com>
2021-11-19 13:19:08 -0300

    Update

--- .local/scripts/utils/extractor.sh
+++ .local/scripts/utils/extractor.sh
@@ -11,12 +11,12 @@ ex ()
             *.tar.gz)    tar xzf $1   ;;
 	    *.tar.xz)	 tar -xf $1   ;;
             *.bz2)       bunzip2 $1   ;;
-            *.rar)       unrar x $1     ;;
+            *.rar)       unrar e $1     ;;
             *.gz)        gunzip $1    ;;
             *.tar)       tar xf $1    ;;
             *.tbz2)      tar xjf $1   ;;
             *.tgz)       tar xzf $1   ;;
-            *.zip)       unzip $1     ;;
+            *.zip)       unzip "$1"     ;;
             *.Z)         uncompress $1;;
             *.7z)        7z x $1      ;;
 	    *.rar)	 unrar e $1   ;;
