143e7cec47f742e4aa51b6e9272f2e76a56062cc
Scherer <vicentin.py@gmail.com>
2021-12-14 22:50:10 -0300

    update arch script

--- .local/scripts/distro-hop/arch.sh
+++ .local/scripts/distro-hop/arch.sh
@@ -37,7 +37,7 @@ install_dwm () {
 }
 
 install_fonts () {
-	# install_packages "adobe-source-han-sans-cn-fonts adobe-source-han-sans-hk-fonts adobe-source-han-sans-jp-fonts adobe-source-han-sans-kr-fonts adobe-source-han-sans-otc-fonts adobe-source-han-sans-tw-fonts adobe-source-han-serif-cn-fonts adobe-source-han-serif-jp-fonts adobe-source-han-serif-kr-fonts adobe-source-han-serif-otc-fonts adobe-source-han-serif-tw-fonts"
+	install_packages "adobe-source-han-sans-cn-fonts adobe-source-han-sans-hk-fonts adobe-source-han-sans-jp-fonts adobe-source-han-sans-kr-fonts adobe-source-han-sans-otc-fonts adobe-source-han-sans-tw-fonts adobe-source-han-serif-cn-fonts adobe-source-han-serif-jp-fonts adobe-source-han-serif-kr-fonts adobe-source-han-serif-otc-fonts adobe-source-han-serif-tw-fonts"
 	yay -S nerd-fonts-complete
 }
 
