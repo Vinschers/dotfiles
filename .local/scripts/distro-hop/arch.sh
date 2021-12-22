e66bb3965416aadb2c4db5f5e944c322cfca463c
Scherer <vicentin.py@gmail.com>
2021-12-21 22:48:48 -0300

    Update

--- .local/scripts/distro-hop/arch.sh
+++ .local/scripts/distro-hop/arch.sh
@@ -34,13 +34,19 @@ install_general () {
 	install_packages "htop gtop neofetch openssh shellcheck"
 }
 
+install_dwm_fonts () {
+	install_packages "ttf-font-awesome ttf-joypixels noto-fonts-emoji otf-latinmodern-math adobe-source-code-pro-fonts"
+}
+
 install_dwm () {
 	install_packages "imlib2 pamixer dunst simple-scan flameshot"
 	yay -S libxft-bgra colorpicker
+
+    check "Install dwm related fonts?" 1 && install_dwm_fonts ; espaco
 }
 
 install_fonts () {
-	install_packages "ttf-font-awesome ttf-joypixels noto-fonts-emoji otf-latinmodern-math adobe-source-code-pro-fonts"
+	install_packages "adobe-source-han-sans-cn-fonts adobe-source-han-sans-hk-fonts adobe-source-han-sans-jp-fonts adobe-source-han-sans-kr-fonts adobe-source-han-sans-otc-fonts adobe-source-han-sans-tw-fonts adobe-source-han-serif-cn-fonts adobe-source-han-serif-jp-fonts adobe-source-han-serif-kr-fonts adobe-source-han-serif-otc-fonts adobe-source-han-serif-tw-fonts"
 }
 
 install_latex () {
@@ -51,13 +57,13 @@ install_gui () {
 	install_packages "xorg xorg-xinit xf86-video-intel xf86-input-wacom picom nvidia nvidia-utils nvidia-settings nitrogen slock pulseaudio pavucontrol alsa-utils alsa-firmware thunar"
 	yay -S picom-jonaburg-git
 
+	check "Install dwm related packages?" 1 && install_dwm ; espaco
+	check "Install additional fonts?" 1 && install_fonts ; espaco
+    check "Install LaTeX packages?" 1 && install_latex ; espaco
+
 	amixer sset Master unmute
 	pulseaudio --check
 	pulseaudio -D
-
-	check "Install dwm related packages?" 1 && install_dwm ; espaco
-	check "Install additional fonts?" 1 && install_fonts ; espaco
-  check "Install LaTeX packages?" 1 && install_latex ; espaco
 }
 
 cp_xorg () {
