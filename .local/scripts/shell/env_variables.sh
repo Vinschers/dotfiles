dbb55b26c041a985fb04e2bf6d041097d5cea4da
Scherer <vicentin.py@gmail.com>
2022-01-02 14:09:52 -0300

    Fix env_variables script

---
+++ .local/scripts/shell/env_variables.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+export SCRIPTS_DIR=~/.local/scripts
+
+export TERMINAL=st
+export EDITOR=nvim
+export LOCK_TIME=15
+
+export SEMESTER=3
+export WALLPAPERS_DIR=~/Media/wallpapers
+
+
+PATH="$PATH:~/.local/bin:$SCRIPTS_DIR/statusbar:$SCRIPTS_DIR/utils"
--- .local/scripts/shellrc.sh
+++ .local/scripts/shellrc.sh
@@ -8,5 +8,4 @@ source_directory() {
 	for script in "$directory"*; do . "$script"; done
 }
 
-. "$SCRIPTS_DIR"/shell/env_variables.sh
 source_directory "$SCRIPTS_DIR"/shell/
