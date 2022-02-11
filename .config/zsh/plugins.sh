7931277967ed525ad97d07deea30ab7837dd3d85
Scherer <vicentin.py@gmail.com>
2022-02-11 12:41:03 -0300

    Add Alt+arrows keybinds

--- .config/zsh/plugins.sh
+++ .config/zsh/plugins.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-#zsh_add_plugin "zsh-users/zsh-autosuggestions"
+zsh_add_plugin "zsh-users/zsh-autosuggestions"
 zsh_add_plugin "zsh-users/zsh-syntax-highlighting"
 #zsh_add_plugin "hlissner/zsh-autopair"
 zsh_add_plugin "jeffreytse/zsh-vi-mode"
+#zsh_add_plugin "custom/zsh-dirhistory"
 
 ZVM_INSERT_MODE_CURSOR=$ZVM_CURSOR_BEAM
