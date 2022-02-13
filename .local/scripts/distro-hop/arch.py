85a8ef0175af91489cbc08ba4bd8d1a328a657a9
Scherer <42344207+Vinschers@users.noreply.github.com>
2022-02-13 17:39:29 -0300

    Create arch.py

---
+++ .local/scripts/distro-hop/arch.py
@@ -0,0 +1,9 @@
+import os
+
+with open("/home/scherer/.local/scripts/distro-hop/pacman_full") as f:
+  for line in f:
+    os.system(f"sudo pacman -S --needed --no-confirm {line}")
+
+with open("/home/scherer/.local/scripts/distro-hop/yay_full") as f:
+  for line in f:
+    os.system(f"yay -S {line}")
