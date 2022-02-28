61480bdba1ecbbbb917e00274cc62af9505e12f9
Scherer <vincentin.py@gmail.com>
2022-02-28 00:39:09 -0300

    Update localhost server

--- .local/scripts/localhost/server.py
+++ .local/scripts/localhost/server.py
@@ -1,16 +1,23 @@
 import os
-
-from flask import Flask, render_template
-
-
-app = Flask(__name__)
+import http.server
+import socketserver
 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.path = '/index.html'
+        self.path = f'{os.environ.get("SCRIPTS_DIR")}/localhost{self.path}'
+        self.path = self.path.replace(os.environ.get('HOME'), '')
+
+        print('\n', self.path, '\n')
+        return http.server.SimpleHTTPRequestHandler.do_GET(self)
 
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 3160))
-    app.run(host='0.0.0.0', port=port, threaded=True)
+Handler = MyHttpRequestHandler
+
+if __name__ == "__main__":
+    # port = int(os.environ.get("PORT", 3160))
+
+    with socketserver.TCPServer(("", 3160), Handler) as httpd:
+        httpd.serve_forever()
