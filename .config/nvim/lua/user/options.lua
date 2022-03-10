8820dcf0b7cd600f225b5049a89a043418b6fe32
Scherer <vincentin.py@gmail.com>
2022-03-10 01:04:39 -0300

    Update neovim option

--- .config/nvim/lua/user/options.lua
+++ .config/nvim/lua/user/options.lua
@@ -36,5 +36,4 @@ vim.opt.sidescrolloff = 8
 vim.opt.shortmess:append "c"
 
 --vim.cmd "set whichwrap+=<,>,[,],h,l"
-vim.cmd [[set iskeyword+=-]]
 vim.cmd [[set formatoptions-=cro]] -- TODO: this doesn't seem to work
