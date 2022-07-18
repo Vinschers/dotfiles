d9ab7c7f81ed79d53ce7aa36e748716ebc8f258d
Scherer <vincentin.py@gmail.com>
2022-07-18 16:21:42 -0300

    Fixing projects issue

--- .config/nvim/lua/user/alpha.lua
+++ .config/nvim/lua/user/alpha.lua
@@ -27,7 +27,7 @@ dashboard.section.header.val = {
 dashboard.section.buttons.val = {
 	dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
 	dashboard.button("e", " " .. " New file", ":ene <BAR> startinsert <CR>"),
-	dashboard.button("p", " " .. " Find project", ":lua require('telescope').extensions.projects.projects()<CR>"),
+	dashboard.button("p", " " .. " Find project", ":Telescope projects<CR>"),
 	dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
 	dashboard.button("F", " " .. " Find text", ":Telescope live_grep <CR>"),
 	dashboard.button("c", " " .. " Config", ":e ~/.config/nvim/init.lua<CR>:cd %:h<CR>"),
