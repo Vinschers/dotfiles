759246fada707497dae5e773b9c4dd2afba25d6f
Scherer <vincentin.py@gmail.com>
2022-07-22 17:12:52 -0300

    Using .Xresources

--- .local/scripts/shell/env_variables.sh
+++ .local/scripts/shell/env_variables.sh
@@ -13,5 +13,6 @@ export SEMESTER=3
 export WALLPAPERS_DIR="$HOME/Media/wallpapers"
 
 export GCC_COLORS='error=01;31:warning=01;93:note=01;36:caret=01;32:locus=01:quote=01'
+export COLORSCHEME="nord"
 
 PATH="$PATH:$HOME/.local/bin:$SCRIPTS_DIR/bin:$SCRIPTS_DIR/bin/statusbar:$HOME/flutter/bin:/opt/android-sdk/cmdline-tools/latest/bin:/opt/android-sdk/platform-tools"
