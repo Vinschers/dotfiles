96c73d15569ea0a83c489cc96b90c75118ca6ace
Scherer <vincentin.py@gmail.com>
2022-08-11 17:55:56 -0300

    Enabling toolbox layout sync in librewolf

--- .local/scripts/shell/env_variables.sh
+++ .local/scripts/shell/env_variables.sh
@@ -5,12 +5,13 @@ export BASHDIR="$HOME"/.config/bash
 export ZDOTDIR="$HOME"/.config/zsh
 
 export TERMINAL="st"
-export TERMINAL_NAME="st"
 export EDITOR="nvim"
-export LOCK_TIME=15
+export BROWSER="librewolf"
 
 export WALLPAPERS_DIR="$HOME/Media/wallpapers"
 export GCC_COLORS='error=01;31:warning=01;93:note=01;36:caret=01;32:locus=01:quote=01'
 export CURRENT_RESEARCH="$HOME/hdd/academic/unicamp/ic_2022"
+
+export LOCK_TIME=15
 
 PATH="$PATH:$HOME/.local/bin:$SCRIPTS_DIR/bin:$SCRIPTS_DIR/bin/statusbar:$HOME/flutter/bin:/opt/android-sdk/cmdline-tools/latest/bin:/opt/android-sdk/platform-tools"
