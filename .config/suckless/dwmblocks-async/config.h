62270ef34b7301f27f81bb8274094592cee68e85
Scherer <vincentin.py@gmail.com>
2022-10-12 01:55:40 -0300

    Fix hololive script

--- .config/suckless/dwmblocks-async/config.h
+++ .config/suckless/dwmblocks-async/config.h
@@ -6,7 +6,8 @@
 const Block blocks[] = {
 	// BLOCK("sb-network",     1,      8),
 	// BLOCK("sb-price",       60,     7),
-	BLOCK("sb-weather",     60,     6),
+	BLOCK("sb-weather",     60,     7),
+	BLOCK("sb-hololive",    60,     6),
 	BLOCK("sb-keyboard",    0,      5),
 	BLOCK("sb-volume",      0,      4),
 	BLOCK("sb-hardware",    2,      3),
