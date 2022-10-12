8f6680f1563646d4477b3cbb13c3dc6c2f679a62
Scherer <vincentin.py@gmail.com>
2022-10-12 15:35:51 -0300

    Add concealment to LaTeX

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -258,7 +258,7 @@ return packer.startup(function(use)
 	use({
 		"lervag/vimtex",
 		config = function()
-			vim.g.vimtex_view_method = "zathura"
+			vim.g.vimtex_view_method = "sioyek"
 			vim.g.vimtex_imaps_enabled = 0
 		end,
 	})
