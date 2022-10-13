b5ab93411f9068b436ee364e8a11b2ce1245ba68
Scherer <vincentin.py@gmail.com>
2022-10-13 12:31:16 -0300

    Fix : in sci_pdf_path

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -309,8 +309,6 @@ return packer.startup(function(use)
 		requires = "RishabhRD/popfix",
 	})
 
-    use({"mong8se/actually.nvim"})
-
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
