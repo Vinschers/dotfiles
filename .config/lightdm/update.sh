d974044a4ac4386626f682a4f7070c176ca4b030
Scherer <vincentin.py@gmail.com>
2022-12-30 19:00:38 -0300

    Using .face instead of AccountsService directory

--- .config/lightdm/update.sh
+++ .config/lightdm/update.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 sudo cp -r "$HOME/.config/lightdm" /etc/
-sudo cp "$HOME/.config/lightdm/icon.png" "/var/lib/AccountsService/icons/$(whoami).png"
+cp "$HOME/.config/lightdm/icon.png" "$HOME/.face"
 [ -d /usr/share/backgrounds ] && sudo rm -rf /usr/share/backgrounds/*
 sudo cp "$HOME"/.config/loginscreen.* /usr/share/backgrounds/
 
-sudo [ -f "/var/lib/AccountsService/users/$(whoami)" ] && sudo sed -i "/Icon=/c\Icon=/var/lib/AccountsService/icons/$(whoami).png" "/var/lib/AccountsService/users/$(whoami)"
+sudo chmod 701 "$HOME"
