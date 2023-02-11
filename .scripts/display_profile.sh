f0d202a064243e7c4f2aa9661eb5c31a9c1da644
Scherer <vicentin.py@gmail.com>
2023-02-10 23:56:32 -0300

    Add wallpaper to hyprland

--- .scripts/display_profile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Script run when starting Xorg or Wayland
-
-dunst &
-wallpaper="$(ls "$HOME/.config" | grep wallpaper | head -n 1)"
-chwall "$HOME/.config/$wallpaper"
-#python "$SCRIPTS_DIR/external/inkscape-shortcut-manager/main.py" &
