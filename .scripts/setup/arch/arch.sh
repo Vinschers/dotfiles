aa143f0f23a4b4b3e1c600ae5e9c0853de68a35f
Felipe <vicentin.py@gmail.com>
2023-02-13 15:45:18 -0300

    Add alsa back

--- .scripts/setup/arch/arch.sh
+++ .scripts/setup/arch/arch.sh
@@ -36,6 +36,8 @@ install_packages() {
 		sudo pacman -S --noconfirm --needed "$PACKAGE" || errors="$errors $PACKAGE"
 	done <"$THIS_DIRECTORY/pacman"
 
+    amixer sset Master unmute
+
 	while read -r PACKAGE; do
 		printf "\n\n\nInstalling %s..." "$PACKAGE"
 		yay --noconfirm -S "$PACKAGE" || errors="$errors $PACKAGE"
