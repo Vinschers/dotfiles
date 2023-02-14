bebdb562aa3d0b0540de3daf1f860913bda470e8
Felipe <vicentin.py@gmail.com>
2023-02-14 02:01:43 -0300

    Fix some scripts for single monitor setup

--- .scripts/hyprland/move_hyprland_workspace.sh
+++ .scripts/hyprland/move_hyprland_workspace.sh
@@ -3,6 +3,6 @@
 ws="$1"
 current_mon="$(cat "/tmp/hypr/$HYPRLAND_INSTANCE_SIGNATURE/active_monitor")"
 
-[ -n "$current_mon" ] && ws="$(( current_mon + 1 ))$ws"
+[ -n "$current_mon" ] && ws="$(( current_mon + 1 ))$(printf "%s" "$ws" | tail -c 1)"
 
 hyprctl dispatch movetoworkspace "$ws"
