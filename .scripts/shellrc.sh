1459b2be0f8fed7450bc1c20bf65af15d35060a8
Felipe <vicentin.py@gmail.com>
2023-02-19 00:40:27 -0300

    [exa] Change EXA_COLORS

--- .scripts/shellrc.sh
+++ .scripts/shellrc.sh
@@ -8,8 +8,8 @@ source_directory() {
 	for script in "$directory"*; do . "$script"; done
 }
 
-source_directory "$SCRIPTS_DIR"/shell/
-
 eval "$(dircolors -b "$SCRIPTS_DIR/shell/.dir_colors")"
+
+source_directory "$SCRIPTS_DIR"/shell/
 
 [ -f "$HOME/.cache/post_setup" ] || "$SCRIPTS_DIR/setup/post_setup.sh"
