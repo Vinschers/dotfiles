ebf1f8d8bb88cc5a9ba2545ddf1d77a15b29f2a4
Felipe <github.k9qcx@slmail.me>
2023-02-25 02:39:19 -0300

    [Hyprland] Support for workspaces 1-10

--- .scripts/hyprland/move_hyprland_workspace.sh
+++ .scripts/hyprland/move_hyprland_workspace.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 ws="$1"
-current_mon="$(hyprctl -j monitors | jaq '.[]? | select(.focused == true) | .id')"
 
-[ -n "$current_mon" ] && ws="$(( current_mon + 1 ))$(printf "%s" "$ws" | tail -c 1)"
+if [ "$(hyprctl monitors | grep -c "Monitor")" -gt 1 ]; then
+	current_mon="$(hyprctl -j monitors | jaq '.[]? | select(.focused == true) | .id')"
+	[ -n "$current_mon" ] && ws="$((current_mon + 1))$(printf "%s" "$ws" | tail -c 1)"
+fi
 
 hyprctl dispatch movetoworkspace "$ws"
-
-eww update update="ok"
