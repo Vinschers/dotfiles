84d17513e74b1016da930c0fb89ce9d92593bb08
Felipe <github.k9qcx@slmail.me>
2023-06-05 12:38:31 -0300

    Fix todo-comments

--- .config/nvim/lua/user/todo-comments.lua
+++ .config/nvim/lua/user/todo-comments.lua
@@ -1,32 +1,11 @@
 local M = {
 	"folke/todo-comments.nvim",
 	dependencies = { "nvim-lua/plenary.nvim" },
-	opts = {
-		{
-			keywords = {
-				FIX = {
-					icon = " ", -- icon used for the sign, and in search results
-					color = "error", -- can be a hex color, or a named color (see below)
-					alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
-					-- signs = false, -- configure signs for some keywords individually
-				},
-				TODO = { icon = " ", color = "DiagnosticSignInfo" },
-				HACK = { icon = " ", color = "DiagnosticSignWarn" },
-				WARN = { icon = " ", color = "DiagnosticSignWarn", alt = { "WARNING", "XXX" } },
-				PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
-				NOTE = { icon = " ", color = "DiagnosticSignHint", alt = { "INFO" } },
-				TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
-			},
-			colors = {
-				error = { "DiagnosticSignError", "ErrorMsg", "#DC2626" },
-				warning = { "DiagnosticSignWarn", "WarningMsg", "#FBBF24" },
-				info = { "DiagnosticSignInfo", "#2563EB" },
-				hint = { "DiagnosticSignHint", "#10B981" },
-				default = { "Identifier", "#7C3AED" },
-				test = { "Identifier", "#FF00FF" },
-			},
-		},
-	},
+    event = "VeryLazy",
 }
+
+function M.config()
+    require("todo-comments").setup()
+end
 
 return M
