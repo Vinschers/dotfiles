7cdb10bcecd368447c8bb4eaa4fda7ce1afbfa67
Felipe <github.k9qcx@slmail.me>
2023-06-10 14:51:38 -0300

    Update bezier curvers

--- .config/themes/update_wallpaper.sh
+++ .config/themes/update_wallpaper.sh
@@ -7,15 +7,22 @@ wallpaper_name="$(cat "$themes_dir/wallpaper")"
 if [ -z "$1" ] || [ "$1" = "n" ]; then
 	next_wallpaper="$(/usr/bin/ls -1 "$themes_dir/wallpapers" | grep -A1 "$wallpaper_name" | grep -v "$wallpaper_name")"
 	[ -z "$next_wallpaper" ] && next_wallpaper="$(/usr/bin/ls -1 "$themes_dir/wallpapers" | head -1)"
+
     effect="grow"
+    bezier="0.33,0,1,0.4"
 elif [ "$1" = "p" ]; then
 	next_wallpaper="$(/usr/bin/ls -1 "$themes_dir/wallpapers" | grep -B1 "$wallpaper_name" | grep -v "$wallpaper_name")"
 	[ -z "$next_wallpaper" ] && next_wallpaper="$(/usr/bin/ls -1 "$themes_dir/wallpapers" | tail -1)"
+
     effect="outer"
+    bezier="0,0.33,0.4,1"
 else
     next_wallpaper="$wallpaper_name"
+
+    effect="grow"
+    bezier="0.33,0,1,0.4"
 fi
 
 echo "$next_wallpaper" >"$themes_dir/wallpaper"
 
-swww img "$themes_dir/wallpapers/$next_wallpaper" --transition-bezier .33,0,1,0.2 --transition-type "$effect" --transition-duration 1 --transition-fps 60 --transition-pos 0.917,0.968
+swww img "$themes_dir/wallpapers/$next_wallpaper" --transition-bezier "$bezier" --transition-type "$effect" --transition-duration 1 --transition-fps 60 --transition-pos 0.917,0.968
