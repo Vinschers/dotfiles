ed46d32a554a358d435ff4577a5c13762065c2c2
Felipe <github.k9qcx@slmail.me>
2023-06-29 17:52:52 -0300

    Refactor path script

--- .config/shell/environment/path.sh
+++ .config/shell/environment/path.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
+add_to_path() {
+    echo "$HOME/.local/bin"
+    echo "$SCRIPTS_DIR"
+    echo "$SCRIPTS_DIR/bin"
+    echo "$SCRIPTS_DIR/bin/statusbar"
+    echo "/opt/android-sdk/cmdline-tools/latest/bin:/opt/android-sdk/platform-tools"
+}
 
-PATH="$HOME/.local/bin:$SCRIPTS_DIR:$SCRIPTS_DIR/bin:$SCRIPTS_DIR/bin/statusbar:/opt/android-sdk/cmdline-tools/latest/bin:/opt/android-sdk/platform-tools:$PATH"
+PATH="$(add_to_path | paste -sd':' | tr -d '\n'):$PATH"
