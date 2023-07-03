202d7df553a21e99771a722a2d390ca4fb4e3be5
Vinschers <github.90eel@slmail.me>
2023-07-03 14:12:58 -0300

    Test arch role creating

--- .config/setup/setup.sh
+++ .config/setup/setup.sh
@@ -4,7 +4,7 @@ curl -sSL https://raw.githubusercontent.com/Vinschers/dotfiles/master/.config/sh
 
 sudo pacman --noconfirm --needed -S ansible-core ansible
 
-mkdir -p "$HOME/.config/setup"
+mkdir -p "$HOME/.config/setup/roles/arch/tasks"
 [ -f "$HOME/.config/setup/requirements.yml" ] || curl -sSL https://raw.githubusercontent.com/Vinschers/dotfiles/master/.config/setup/requirements.yml >"$HOME/.config/setup/requirements.yml"
 [ -f "$HOME/.config/setup/bootstrap.yml" ] || curl -sSL https://raw.githubusercontent.com/Vinschers/dotfiles/master/.config/setup/bootstrap.yml >"$HOME/.config/setup/bootstrap.yml"
 
