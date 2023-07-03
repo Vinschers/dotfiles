16592235e1b740aaf3b363607094a4025c9839ed
Vinschers <github.90eel@slmail.me>
2023-07-03 03:47:28 -0300

    Move nvidia config to nvidia.yml

--- .config/setup/setup.sh
+++ .config/setup/setup.sh
@@ -21,28 +21,6 @@ lspci -k | grep -A 2 -E "(VGA|3D)" | grep -qi nvidia && NVIDIA=1
 ansible-playbook --ask-become-pass "$HOME/.config/setup/bootstrap.yml"
 
 yay --noconfirm --cleanafter --needed -S vscode-langservers-extracted
-yay --noconfirm --cleanafter --needed -S vscode-node-debug2
-
-if [ "$NVIDIA" = "1" ]; then
-	if ! grep -q "nvidia_drm.modeset=1" /etc/default/grub; then
-		sudo sed -i '/^GRUB_CMDLINE_LINUX_DEFAULT=/ s/"$/ nvidia_drm.modeset=1"/g' /etc/default/grub
-		grub-mkconfig -o /boot/grub/grub.cfg
-	fi
-
-	if ! grep -q "nvidia nvidia_modeset nvidia_uvm nvidia_drm" /etc/mkinitcpio.conf; then
-		default_modules="$(grep "^MODULES=" /etc/mkinitcpio.conf)"
-		modified_modules="${default_modules%?} nvidia nvidia_modeset nvidia_uvm nvidia_drm)"
-		modified_modules="$(echo "$modified_modules" | sed 's/( /(/g')"
-
-		sudo sed -i "s|^$default_modules|$modified_modules|g" /etc/mkinitcpio.conf
-		sudo mkinitcpio -P
-	fi
-
-	if ! grep -q "options nvidia-drm modeset=1" /etc/modprobe.d/nvidia.conf; then
-		echo "options nvidia-drm modeset=1" | sudo tee -a /etc/modprobe.d/nvidia.conf
-	fi
-
-fi
 
 printf "Setup SSD trim? [Y/n] "
 read -r ssd
