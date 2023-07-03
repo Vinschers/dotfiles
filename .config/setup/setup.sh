4ed537de45f1190472d3f62e9c9d050dac7f8235
Vinschers <github.90eel@slmail.me>
2023-07-03 13:58:39 -0300

    Add files if missing ansible

--- .config/setup/setup.sh
+++ .config/setup/setup.sh
@@ -4,5 +4,8 @@ curl -sSL https://raw.githubusercontent.com/Vinschers/dotfiles/master/.config/sh
 
 sudo pacman --noconfirm --needed -S ansible-core ansible
 
+[ -f "$HOME/.config/setup/requirements.yml" ] || curl -sSL https://raw.githubusercontent.com/Vinschers/dotfiles/master/.config/setup/requirements.yml >"$HOME/.config/setup/requirements.yml"
+[ -f "$HOME/.config/setup/bootstrap.yml" ] || curl -sSL https://raw.githubusercontent.com/Vinschers/dotfiles/master/.config/setup/bootstrap.yml >"$HOME/.config/setup/bootstrap.yml"
+
 ansible-galaxy collection install -r "$HOME/.config/setup/requirements.yml"
 ansible-playbook --ask-become-pass "$HOME/.config/setup/bootstrap.yml"
