fc375fda5b2e7a8e82f219dd653690248aec93cc
Vinschers <github.90eel@slmail.me>
2023-07-03 15:19:27 -0300

    grep -E instead of egrep

--- .config/setup/setup.sh
+++ .config/setup/setup.sh
@@ -3,7 +3,7 @@
 if ! [ -d "$HOME/.config/.dotfiles-git" ]; then
 	git clone --bare https://github.com/Vinschers/dotfiles.git "$HOME/.config/.dotfiles-git"
 
-	git --git-dir="$HOME/.config/.dotfiles-git/" --work-tree="$HOME" checkout 2>&1 | egrep "\s+\." | awk '{print $1}' | xargs -I{} rm "$HOME/{}"
+	git --git-dir="$HOME/.config/.dotfiles-git/" --work-tree="$HOME" checkout 2>&1 | grep -E "\s+\." | awk '{print $1}' | xargs -I{} rm "$HOME/{}"
 	git --git-dir="$HOME/.config/.dotfiles-git/" --work-tree="$HOME" checkout
 
 	git --git-dir="$HOME/.config/.dotfiles-git/" --work-tree="$HOME" config --local status.showUntrackedFiles no
