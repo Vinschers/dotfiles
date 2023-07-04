e28833783dba86f3c38378c02ce8ffc20a38bb24
Felipe <vicentin.py@gmail.com>
2023-07-04 15:48:29 -0300

    Try installing hyprload plugins automatically

--- .config/hypr/hyprload.sh
+++ .config/hypr/hyprload.sh
@@ -2,4 +2,8 @@
 
 if ! [ -d "$HOME/.local/share/hyprload" ]; then
     curl -sSL https://raw.githubusercontent.com/Duckonaut/hyprload/main/install.sh | bash
+    sleep 1
+    hyprctl reload
+    sleep 1
+    hyprctl dispatch hyprload update
 fi
