347e6ee634e3f947efa7d8cb207991aec798e5cb
Felipe <github.k9qcx@slmail.me>
2023-07-05 00:51:22 -0300

    Fix man colors

--- .config/shell/environment/options.sh
+++ .config/shell/environment/options.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 
-# export MOZ_ENABLE_WAYLAND=1
+# Have less display colours
+export LESS_TERMCAP_mb="$(tput bold; tput setaf 2)"
+export LESS_TERMCAP_md="$(tput bold; tput setaf 6)"
+export LESS_TERMCAP_so="$(tput bold; tput setaf 3; tput setab 4)"
+export LESS_TERMCAP_us="$(tput bold; tput setaf 7)"
+export LESS_TERMCAP_me="$(tput sgr0)"
+export LESS_TERMCAP_se="$(tput rmso; tput sgr0)"
+export LESS_TERMCAP_ue="$(tput sgr0)"
+export LESS_TERMCAP_mr="$(tput rev)"
+export LESS_TERMCAP_mh="$(tput dim)"
+export LESS_TERMCAP_ZN="$(tput ssubm)"
+export LESS_TERMCAP_ZV="$(tput rsubm)"
+export LESS_TERMCAP_ZO="$(tput ssupm)"
+export LESS_TERMCAP_ZW="$(tput rsupm)"
--- .config/shell/environment/variables.sh
+++ .config/shell/environment/variables.sh
@@ -3,5 +3,3 @@
 export GCC_COLORS='error=01;31:warning=01;93:note=01;36:caret=01;32:locus=01:quote=01'
 
 export LOCK_TIME=15
-
-export MANPAGER="sh -c 'col -bx | bat -l man -p'"
