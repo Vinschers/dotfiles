b7a0c7729625996c396e262f8f1b6cdbad59f820
Felipe <github.k9qcx@slmail.me>
2023-07-06 03:37:00 -0300

    Add theme buttons

--- .config/nvim/lua/config/autocommands.lua
+++ .config/nvim/lua/config/autocommands.lua
@@ -73,6 +73,12 @@ vim.api.nvim_create_autocmd({ "BufWritePost" }, {
 		os.execute("xrdb ~/.Xresources 2> /dev/null")
 	end,
 })
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+	pattern = { "eww.scss" },
+	callback = function()
+		os.execute("eww reload")
+	end,
+})
 vim.api.nvim_create_autocmd({ "BufEnter" }, {
 	pattern = { "*.Xresources" },
 	callback = function()
