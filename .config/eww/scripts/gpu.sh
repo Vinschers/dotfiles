c8a8f3579bd7abd84063abb0afb7ec074db70c12
Felipe <github.k9qcx@slmail.me>
2023-07-11 01:51:20 -0300

    Fix bugs in eww

--- .config/eww/scripts/gpu.sh
+++ .config/eww/scripts/gpu.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-if lspci -k | grep -A 2 -E "(VGA|3D)" | grep -qi nvidia; then
-    command -v "nvidia-smi" >/dev/null 2>&1 || exit
-    nvidia-smi -q -d UTILIZATION | awk -F: '/Avg/ {print $2}' | grep -Po '[0-9]+' -m 1
-elif lspci -k | grep -A 2 -E "(VGA|3D)" | grep -qi amd; then
-    radeontop --limit 1 --dump - | grep -oP '(?<=vram )([0-9]+.[0-9]+)'
-fi
+while true; do
+	if lspci -k | grep -A 2 -E "(VGA|3D)" | grep -qi nvidia; then
+		command -v "nvidia-smi" >/dev/null 2>&1 || exit
+		nvidia-smi -q -d UTILIZATION | awk -F: '/Avg/ {print $2}' | grep -Po '[0-9]+' -m 1
+	elif lspci -k | grep -A 2 -E "(VGA|3D)" | grep -qi amd; then
+		radeontop --limit 1 --dump - | grep -oP '(?<=vram )([0-9]+.[0-9]+)'
+	fi
+
+    sleep 1
+done
--- .config/eww/scripts/weather.sh
+++ .config/eww/scripts/weather.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 
-icon="$(curl -s 'wttr.in?format=%c' | sed 's/ //g')"
-json="$(curl -s 'wttr.in?format=j1' | jaq -c '{temp: .current_condition[0].temp_C, feels: .current_condition[0].FeelsLikeC, location: .nearest_area[0].areaName[0].value, desc: .current_condition[0].weatherDesc[0].value}')"
+icon="$(curl --silent --max-time 2 'wttr.in?format=%c' | sed 's/ //g')"
+json="$(curl --silent --max-time 2 'wttr.in?format=j1' | jaq -c '{temp: .current_condition[0].temp_C, feels: .current_condition[0].FeelsLikeC, location: .nearest_area[0].areaName[0].value, desc: .current_condition[0].weatherDesc[0].value}')"
 
 echo "$json" | jaq -c ". += {icon: \"$icon\"}"
