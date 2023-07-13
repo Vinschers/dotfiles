ce2465e5781e6e2015aab3109d8c9c9c49fd4b90
Felipe <github.k9qcx@slmail.me>
2023-07-13 15:27:21 -0300

    Remove nord and setup spicetify

--- .config/eww/scripts/weather.sh
+++ .config/eww/scripts/weather.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-
 icon="$(curl --silent --max-time 2 'wttr.in?format=%c' | sed 's/ //g')"
-json="$(curl --silent --max-time 2 'wttr.in?format=j1' | jaq -c '{temp: .current_condition[0].temp_C, feels: .current_condition[0].FeelsLikeC, location: .nearest_area[0].areaName[0].value, desc: .current_condition[0].weatherDesc[0].value}')"
+json="$(curl --silent --max-time 2 'wttr.in?format=j1' | jq -c '{temp: .current_condition[0].temp_C, feels: .current_condition[0].FeelsLikeC, location: .nearest_area[0].areaName[0].value, desc: .current_condition[0].weatherDesc[0].value}')"
 
-echo "$json" | jaq -c ". += {icon: \"$icon\"}"
+echo "$json" | jaq -rc ". += {icon: \"$icon\"}"
