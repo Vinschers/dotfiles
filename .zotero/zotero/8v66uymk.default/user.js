d87f78aba5f965facc987b59af779b8670ab8d67
Felipe <github.k9qcx@slmail.me>
2023-08-24 11:43:57 -0300

    Test zotero config

---
+++ .zotero/zotero/8v66uymk.default/user.js
@@ -0,0 +1,4 @@
+user_pref("extensions.zotero.fileHandler.pdf", "system");
+
+user_pref("extensions.zotero.translators.better-bibtex.citekeyFormat", "auth.lower + '_' + shorttitle.lower + year");
+user_pref("extensions.zotero.translators.better-bibtex.citekeyFormatEditing", "auth.lower + '_' + shorttitle.lower + year");
