f7df4a0d99b13ebe6c7e7e2a72ec92ac17197dc8
Felipe <github.k9qcx@slmail.me>
2023-08-24 12:37:03 -0300

    Add zotero config

--- .zotero/zotero/8v66uymk.default/user.js
+++ .zotero/zotero/8v66uymk.default/user.js
@@ -1,4 +1,13 @@
+user_pref("extensions.zotero.dataDir", "/home/scherer/.local/share/Zotero");
 user_pref("extensions.zotero.fileHandler.pdf", "system");
 
-user_pref("extensions.zotero.translators.better-bibtex.citekeyFormat", "auth.lower + '_' + shorttitle.lower + year");
-user_pref("extensions.zotero.translators.better-bibtex.citekeyFormatEditing", "auth.lower + '_' + shorttitle.lower + year");
+user_pref("extensions.zotero.translators.better-bibtex.citekeyFormat", "auth.lower + '_' + shorttitle(1).lower + year");
+user_pref("extensions.zotero.translators.better-bibtex.citekeyFormatEditing", "auth.lower + '_' + shorttitle(1).lower + year");
+
+user_pref("extensions.zotfile.dest_dir", "/home/scherer/academic/bibliography");
+user_pref("extensions.zotfile.import", false);
+user_pref("extensions.zotfile.source_dir", "/home/scherer/academic/bibliography");
+user_pref("extensions.zotfile.source_dir_ff", false);
+user_pref("extensions.zotfile.subfolder", true);
+user_pref("extensions.zotfile.subfolderFormat", "/%T");
+user_pref("extensions.zotfile.useZoteroToRename", true);
