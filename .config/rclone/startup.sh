34f2427d0fc680169701cf33391712f79a3c4140
Felipe <github.k9qcx@slmail.me>
2023-08-29 16:11:09 -0300

    quiet on hyprload & rclone on modify

--- .config/rclone/startup.sh
+++ .config/rclone/startup.sh
@@ -4,7 +4,6 @@
 rclone copy academic: "$ACADEMIC_DIRECTORY" --exclude-from "$HOME/.config/rclone/ignore.txt"
 
 # Update cloud directory periodically
-while true; do
-    rclone copy "$ACADEMIC_DIRECTORY" academic: --exclude-from "$HOME/.config/rclone/ignore.txt"
-    sleep 300 # Every 5 min
+inotifywait -m "$ACADEMIC_DIRECTORY" -e close_write -e moved_to -e move_self -e create | while read -r dir action file; do
+	rclone copy "$ACADEMIC_DIRECTORY" academic: --exclude-from "$HOME/.config/rclone/ignore.txt"
 done
