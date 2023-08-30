c392b50b30d0187071a8e589b76b1e8565608ddf
Felipe <github.k9qcx@slmail.me>
2023-08-29 21:43:15 -0300

    Add .stignore

--- .config/hypr/scripts/startup.sh
+++ .config/hypr/scripts/startup.sh
@@ -20,5 +20,5 @@ for monitor in $(seq "$(( num_monitors - 1 ))" -1 0); do
     eww open "bar_window$monitor"
 done
 
-"$HOME"/.config/rclone/startup.sh &
+# "$HOME"/.config/rclone/startup.sh &
 "$HOME"/.config/theme/switch_theme.sh &
