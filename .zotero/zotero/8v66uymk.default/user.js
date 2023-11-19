ed0555ae6138f9480b0b705042797be9a10c8f04
Felipe <github.k9qcx@slmail.me>
2023-11-19 03:27:09 -0300

    Update zotero

--- .zotero/zotero/8v66uymk.default/user.js
+++ .zotero/zotero/8v66uymk.default/user.js
@@ -5,12 +5,16 @@ user_pref("extensions.zotero.fileHandler.pdf", "system");
 
 user_pref("extensions.zotero.translators.better-bibtex.citekeyFormat", "auth.lower + '_' + shorttitle(1).lower + '_' + year");
 user_pref("extensions.zotero.translators.better-bibtex.citekeyFormatEditing", "auth.lower + '_' + shorttitle(1).lower + '_' + year");
+user_pref("extensions.zotero.translators.better-bibtex.skipFields", "file");
 
-user_pref("extensions.zotfile.dest_dir", "/home/scherer/academic/bibliography");
+user_pref("extensions.zotfile.dest_dir", "/home/scherer/academic/");
 user_pref("extensions.zotfile.import", false);
-user_pref("extensions.zotfile.source_dir", "/home/scherer/academic/bibliography");
+user_pref("extensions.zotfile.source_dir", "/home/scherer/academic/");
 user_pref("extensions.zotfile.source_dir_ff", false);
+user_pref("extensions.zotfile.useZoteroToRename", false);
 user_pref("extensions.zotfile.subfolder", true);
-user_pref("extensions.zotfile.subfolderFormat", "/%T");
-user_pref("extensions.zotfile.useZoteroToRename", true);
+user_pref("extensions.zotfile.subfolderFormat", "/%c/%T/[%a]{ %t (%y)}");
+user_pref("extensions.zotfile.renameFormat", "[%a]{ %t (%y)}");
+user_pref("extensions.zotfile.authors_delimiter", ", ");
+user_pref("extensions.zotfile.max_titlelength", 120);
 user_pref("extensions.zotfile.automatic_renaming", 4);
