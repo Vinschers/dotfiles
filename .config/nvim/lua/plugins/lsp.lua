13786572a544a1c64b399ff7597e13a08adaee97
Felipe <github.k9qcx@slmail.me>
2023-12-20 11:47:02 -0300

    Fix webcam script

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -67,6 +67,20 @@ return {
 	},
 
 	{
+		"nvimtools/none-ls.nvim",
+		opts = function(_, opts)
+			local nls = require("null-ls")
+			opts.root_dir = opts.root_dir
+				or require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git")
+			opts.sources = vim.list_extend(opts.sources or {}, {
+                nls.builtins.code_actions.shellcheck,
+                nls.builtins.code_actions.refactoring,
+                nls.builtins.code_actions.proselint,
+			})
+		end,
+	},
+
+	{
 		"mfussenegger/nvim-dap",
 		opts = function()
 			local dap = require("dap")
