fea9a07e7c81359246fa31739dddfac6d1fa5bc5
Felipe <github.k9qcx@slmail.me>
2023-12-20 22:44:19 -0300

    Dynamic theme in Kvantum

---
+++ .config/Kvantum/themes/update.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+update_theme() {
+    sed -i "$HOME/.config/Kvantum/kvantum.kvconfig" -e "s/theme=.*/theme=$1/g"
+}
+
+if [ -n "$1" ]; then
+    [ "$1" -eq 1 ] && update_theme "KvArcDark"
+fi
