2ce7c2bc4249f6557374d9acc7a8edb89a97f7e7
Felipe <github.k9qcx@slmail.me>
2023-12-20 23:44:14 -0300

    Remove kvantum.kvconfig

--- .config/Kvantum/themes/update.sh
+++ .config/Kvantum/themes/update.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 update_theme() {
-    sed -i "$HOME/.config/Kvantum/kvantum.kvconfig" -e "s/theme=.*/theme=$1/g"
+    if [ -f "$HOME/.config/Kvantum/kvantum.kvconfig" ]; then
+        sed -i "$HOME/.config/Kvantum/kvantum.kvconfig" -e "s/theme=.*/theme=$1/g"
+    else
+        printf "[General]\ntheme=%s\n" "$1" > "$HOME/.config/Kvantum/kvantum.kvconfig"
+    fi
 }
 
 if [ -n "$1" ]; then
