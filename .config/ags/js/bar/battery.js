d7c9b6abbbebc662ea5a7825607f74ba35f13dc3
Felipe <github.k9qcx@slmail.me>
2023-12-24 15:24:27 -0300

    Start battery

--- .config/ags/js/bar/battery.js
+++ .config/ags/js/bar/battery.js
@@ -1,6 +1,18 @@
-import Widget from "resource:///com/github/Aylur/ags/widget.js";
 import Battery from "resource:///com/github/Aylur/ags/service/battery.js";
+import Widget from "resource:///com/github/Aylur/ags/widget.js";
 
-const BatteryWidget = () => Widget.Box({});
+const BatteryWidget = () =>
+    Widget.CircularProgress({
+        class_name: Battery.bind("charging").transform((ch) => {
+            return `battery ${ch ? "charging" : ""}`;
+        }),
+        child: Widget.Icon({
+            icon: Battery.bind("icon_name").transform(
+                (v) => v?.toString() || "object-select-symbolic",
+            ),
+        }),
+        visible: Battery.bind("available"),
+        value: Battery.bind("percent").transform((p) => (p > 0 ? p / 100 : 0)),
+    });
 
 export default BatteryWidget;
