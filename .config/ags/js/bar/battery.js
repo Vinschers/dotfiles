71c534693ab769fada63494c4c11c33886bd0b58
Felipe <github.k9qcx@slmail.me>
2023-12-24 16:00:35 -0300

    Battery Widget

--- .config/ags/js/bar/battery.js
+++ .config/ags/js/bar/battery.js
@@ -2,17 +2,35 @@ import Battery from "resource:///com/github/Aylur/ags/service/battery.js";
 import Widget from "resource:///com/github/Aylur/ags/widget.js";
 
 const BatteryWidget = () =>
-    Widget.CircularProgress({
-        class_name: Battery.bind("charging").transform((ch) => {
-            return `battery ${ch ? "charging" : ""}`;
-        }),
-        child: Widget.Icon({
-            icon: Battery.bind("icon_name").transform(
-                (v) => v?.toString() || "object-select-symbolic",
+    Widget.Box({
+        class_name: "battery",
+        child: Widget.CircularProgress({
+            class_name: Battery.bind("percent").transform((bat) => {
+                let classes = ["circprog"];
+
+                if (Battery.available) {
+                    // @ts-ignore
+                    const status = [
+                        [101, "full"],
+                        [75, "high"],
+                        [50, "medium"],
+                        [20, "low"],
+                    ].find(([threshold]) => Number(threshold) <= bat)[1];
+
+                    classes.push(status.toString())
+                }
+
+                return classes.join(" ");
+            }),
+            child: Widget.Icon({
+                icon: Battery.bind("icon_name").transform(
+                    (v) => v?.toString() || "object-select-symbolic",
+                ),
+            }),
+            visible: Battery.bind("available"),
+            value: Battery.bind("percent").transform((p) =>
+                p > 0 ? p / 100 : 0,
             ),
         }),
-        visible: Battery.bind("available"),
-        value: Battery.bind("percent").transform((p) => (p > 0 ? p / 100 : 0)),
     });
-
 export default BatteryWidget;
