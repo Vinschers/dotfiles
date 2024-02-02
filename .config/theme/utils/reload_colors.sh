184a06c49b0d3cd18cecb3ad884568e819c0545c
Felipe <github.k9qcx@slmail.me>
2024-02-02 01:17:52 -0300

    Update nord & everforest bright palette

--- .config/theme/utils/reload_colors.sh
+++ .config/theme/utils/reload_colors.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+themes_dir="$HOME/.local/share/themes"
+oomox_colors="$HOME/.config/oomox/oomox-colors"
+/opt/oomox/plugins/theme_oomox/change_color.sh --target-dir "$themes_dir" --output wal "$oomox_colors"
+gsettings set org.gnome.desktop.interface gtk-theme ''
+
 killall -s USR1 cava
 
 spicetify config color_scheme "default" -q
@@ -9,19 +14,14 @@ if pgrep spotify >/dev/null; then
 	sleep 1 && pkill spicetify
 fi
 
-themes_dir="$HOME/.local/share/themes"
-oomox_colors="$HOME/.config/oomox/oomox-colors"
-/opt/oomox/plugins/theme_oomox/change_color.sh --target-dir "$themes_dir" --output wal "$oomox_colors"
-gsettings set org.gnome.desktop.interface gtk-theme ''
-
 ss -a | grep nvim | awk '{print $5}' | while read -r nvim_socket; do
 	nvim --server "$nvim_socket" --remote-send "<esc>:Lazy reload LazyVim<cr>"
 done
 
 ags --run-js "scss();"
 
-gsettings set org.gnome.desktop.interface gtk-theme wal
-
 hyprctl reload
+
+gsettings set org.gnome.desktop.interface gtk-theme wal
 
 wal_steam -w
