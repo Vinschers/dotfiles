0cd5a62da34fccc8eeb28cc78ef66583d68b6098
Felipe <github.k9qcx@slmail.me>
2024-02-02 03:18:12 -0300

    Saving themes for later

--- .config/theme/utils/reload_colors.sh
+++ .config/theme/utils/reload_colors.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
+theme="$(basename "$(readlink -f "$HOME/.config/theme/theme.json")")"
+
 themes_dir="$HOME/.local/share/themes"
-icons_dir="$HOME/.local/share/icons/wal"
+icons_dir="$HOME/.local/share/icons/$theme"
 oomox_colors="$HOME/.config/oomox/oomox-colors"
 
-/opt/oomox/plugins/theme_oomox/change_color.sh --target-dir "$themes_dir" --output wal "$oomox_colors"
-/opt/oomox/plugins/icons_archdroid/archdroid-icon-theme/change_color.sh --destdir "$icons_dir" --output wal "$oomox_colors"
-gsettings set org.gnome.desktop.interface gtk-theme ''
+[ -d "$themes_dir/$theme" ] || /opt/oomox/plugins/theme_oomox/change_color.sh --target-dir "$themes_dir" --output "$theme" "$oomox_colors"
+[ -d "$icons_dir" ] || /opt/oomox/plugins/icons_archdroid/archdroid-icon-theme/change_color.sh --destdir "$icons_dir" --output "$theme" "$oomox_colors"
 
 killall -s USR1 cava
 
@@ -17,16 +18,17 @@ if pgrep spotify >/dev/null; then
 	sleep 1 && pkill spicetify
 fi
 
-ss -a | grep nvim | awk '{print $5}' | while read -r nvim_socket; do
-	nvim --server "$nvim_socket" --remote-send "<esc>:Lazy reload LazyVim<cr>"
-done
-
 pywalfox update
 
 ags --run-js "scss();"
 
 hyprctl reload
 
-gsettings set org.gnome.desktop.interface gtk-theme wal
+gsettings set org.gnome.desktop.interface gtk-theme "$theme"
 
 wal_steam -w
+
+sleep 1
+ss -a | grep nvim | awk '{print $5}' | while read -r nvim_socket; do
+	nvim --server "$nvim_socket" --remote-send "<esc>:Lazy reload LazyVim<cr>"
+done
