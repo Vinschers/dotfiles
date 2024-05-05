089ebcb5951d08e94ede598d3a0c6a4d31e300b7
Felipe <github.k9qcx@slmail.me>
2024-05-05 03:19:26 -0300

    Update setup (Not sure if it works)

--- .config/setup/setup.sh
+++ .config/setup/setup.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-if ! [ -d "$HOME/.config/.dotfiles-git" ]; then
-	git clone --bare https://github.com/Vinschers/dotfiles.git "$HOME/.config/.dotfiles-git"
+clone_git() {
+	[ -d "$HOME/.config/.dotfiles-git" ] && return
+
+	git clone --bare --recursive https://github.com/Vinschers/dotfiles.git "$HOME/.config/.dotfiles-git"
 
 	git --git-dir="$HOME/.config/.dotfiles-git/" --work-tree="$HOME" checkout 2>&1 | grep -E "\s+\." | awk '{print $1}' | xargs -I{} rm "$HOME/{}"
 	git --git-dir="$HOME/.config/.dotfiles-git/" --work-tree="$HOME" checkout
@@ -10,15 +12,32 @@ if ! [ -d "$HOME/.config/.dotfiles-git" ]; then
 	git --git-dir="$HOME/.config/.dotfiles-git/" --work-tree="$HOME" update-index --assume-unchanged "$HOME/.config/shell/environment/local.sh"
 	git --git-dir="$HOME/.config/.dotfiles-git/" --work-tree="$HOME" update-index --assume-unchanged "$HOME/.librewolf/profiles.ini"
 	git --git-dir="$HOME/.config/.dotfiles-git/" --work-tree="$HOME" update-index --assume-unchanged "$HOME/.config/octave/octaverc"
-fi
+}
 
-. "$HOME/.config/shell/environment/xdg.sh"
+run_ansible() {
+	ansible-galaxy collection install -r "$HOME/.config/setup/requirements.yml"
+	ansible-playbook --ask-become-pass "$HOME/.config/setup/bootstrap.yml"
+}
 
-sudo pacman --noconfirm --needed -S ansible-core ansible
+cleanup() {
+	rm -rf "$HOME/.ansible"
+}
 
-ansible-galaxy collection install -r "$HOME/.config/setup/requirements.yml"
-ansible-playbook --ask-become-pass "$HOME/.config/setup/bootstrap.yml"
+distro="$(awk '/^ID=/' /etc/*-release | awk -F'=' '{ print tolower($2) }')"
 
-[ "$(find "$ZDOTDIR"/plugins/* -maxdepth 0 -type d | wc -l)" -eq 1 ] && /bin/zsh
+case "$distro" in
+arch)
+	sudo pacman --noconfirm --needed -S git ansible-core ansible
+	;;
+gentoo)
+    sudo emerge dev-vcs/git app-admin/ansible
+    ;;
+*)
+	exit 1
+	;;
+esac
 
-rm -rf "$HOME/.ansible"
+clone_git
+run_ansible
+cleanup
+[ "$(find "$HOME/.config/zsh/plugins"/* -maxdepth 0 -type d | wc -l)" -eq 1 ] && /bin/zsh
