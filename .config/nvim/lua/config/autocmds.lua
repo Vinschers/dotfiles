4209db78c27302ee7650fb0af5ab2db0dd8329ef
Felipe Scherer <code@a.vicentin.xyz>
2024-07-02 23:59:57 -0300

    Fix LF images

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,38 +1,38 @@
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-	pattern = { "*.Xresources" },
-	callback = function()
-		os.execute("xrdb ~/.Xresources 2> /dev/null")
-	end,
+    pattern = { "*.Xresources" },
+    callback = function()
+        os.execute("xrdb ~/.Xresources 2> /dev/null")
+    end,
 })
 vim.api.nvim_create_autocmd({ "BufEnter" }, {
-	pattern = { "*.Xresources" },
-	callback = function()
-		vim.cmd(":set ft=xdefaults")
-	end,
+    pattern = { "*.Xresources" },
+    callback = function()
+        vim.cmd(":set ft=xdefaults")
+    end,
 })
 
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-	pattern = { "eww.scss" },
-	callback = function()
-		os.execute("eww reload")
-	end,
+    pattern = { "eww.scss" },
+    callback = function()
+        os.execute("eww reload")
+    end,
 })
 
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-	pattern = { "*/ags/*.scss" },
-	callback = function()
-		os.execute('ags --run-js "scss();" >/dev/null')
-	end,
+    pattern = { "*/ags/*.scss" },
+    callback = function()
+        os.execute('ags --run-js "scss();" >/dev/null')
+    end,
 })
 
 vim.api.nvim_create_autocmd("Signal", {
-	pattern = "SIGWINCH",
-	callback = function()
-		package.loaded["plugins.colorscheme"] = nil
-		package.loaded["config.keymaps"] = nil
+    pattern = "SIGWINCH",
+    callback = function()
+        package.loaded["plugins.colorscheme"] = nil
+        package.loaded["config.keymaps"] = nil
 
-		local theme = require("plugins.colorscheme")[1].opts.colorscheme
+        local theme = require("plugins.colorscheme")[1].opts.colorscheme
 
-		vim.keymap.set({ "n" }, "<S-r>", "<Esc><cmd>colorscheme " .. theme .. "<cr>")
-	end,
+        vim.keymap.set({ "n" }, "<S-r>", "<Esc><cmd>colorscheme " .. theme .. "<cr>")
+    end,
 })
