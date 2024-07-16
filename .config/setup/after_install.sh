b8e2ffd9e8c5cfa8c404dd5b5385ef405b05e2df
Felipe <github.k9qcx@slmail.me>
2024-07-16 03:38:02 -0300

    Fix photoshop install script

--- .config/setup/after_install.sh
+++ .config/setup/after_install.sh
@@ -2,20 +2,24 @@
 
 hyprland() {
     hyprpm update
-    echo "y" | hyprpm add https://github.com/Duckonaut/split-monitor-workspaces
-    hyprpm enable split-monitor-workspaces
+    if ! hyprpm list | grep -q split-monitor-workspaces; then
+        echo "y" | hyprpm add https://github.com/Duckonaut/split-monitor-workspaces
+        hyprpm enable split-monitor-workspaces
+    fi
 }
 
 setup_wine() {
-	export WINEDEBUG=-all
+    export WINEDEBUG=-all
 
-	mkdir -p "$WINEPREFIX"
+    mkdir -p "$WINEPREFIX"
 
-	wine hostname
-	winetricks --unattended dotnet20 dotnet48 d3dcompiler_43 d3dx9 d3dx10 d3dx11_42 d3dx11_43 faudio vcrun2005 vcrun2008 vcrun2010 vcrun2012 vcrun2013 vcrun2015 quartz fontsmooth=rgb gdiplus msxml3 msxml6 atmlib
+    wine hostname
+    winetricks --unattended dotnet20 dotnet48 d3dcompiler_43 d3dx9 d3dx10 d3dx11_42 d3dx11_43 faudio vcrun2005 vcrun2008 vcrun2010 vcrun2012 vcrun2013 vcrun2015 quartz fontsmooth=rgb gdiplus msxml3 msxml6 atmlib
 
-	setup_dxvk install
-	setup_vkd3d_proton install
+    if [ "$(lspci -k | grep -A 2 -E "(VGA|3D)" | { grep -i amd || true; })" != "" ]; then
+        setup_dxvk install
+        setup_vkd3d_proton install
+    fi
 }
 
 photoshop() {
--- README.md
+++ README.md
@@ -18,6 +18,17 @@ tutorial. Then, run
 dotfiles remote set-url origin git@github.com:Vinschers/dotfiles.git
 ```
 
+## Post-Installation
+
+Download the Photoshop 2021 archive [here](https://qiwi.gg/file/kMnq2220-AdobePhotoshop2021) and check its SHA256.
+
+```sh
+[ "$(sha256sum AdobePhotoshop2021.tar.xz | awk '{print $1}')" = "8321b969161f2d2ad736067320d493c5b6ae579eaab9400cd1fda6871af2c033" ] && echo ok
+```
+
+Extract the archive to `.config/setup/programs/`
+Run the script at `.config/setup/after_install.sh`
+
 ## Setup wi-fi
 
 First, start the iwd service with `sudo systemctl enable iwd`.
@@ -27,8 +38,13 @@ To do so, install the `dhcpcd` and run `sudo systemctl enable dhcpcd.service`.
 ## Showcase
 
 ### Tokyonight
+
 ![img](.local/share/pictures/tokyonight.png)
+
 ### Everforest
+
 ![img](.local/share/pictures/everforest.png)
+
 ### Nord
+
 ![img](.local/share/pictures/nord.png)
