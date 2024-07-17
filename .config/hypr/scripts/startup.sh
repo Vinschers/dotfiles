8920bcec2f57841d0ecd739d244cf595051223a9
Felipe <github.k9qcx@slmail.me>
2024-07-17 16:58:39 -0300

    Remove annoying red alert in hyprland

---
+++ .config/hypr/scripts/startup.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+hyprpm reload -n
+sleep 0.2
+mainMod="SUPER"
+
+seq 1 10 | while read -r ws; do
+    i="$((ws % 10))"
+    hyprctl keyword bind "$mainMod", "$i", split-workspace, "$ws"
+    hyprctl keyword bind "$mainMod SHIFT", "$i", split-movetoworkspace, "$ws"
+done
+
+hyprctl keyword bind ALT SHIFT, L, split-changemonitor, next
+hyprctl keyword bind ALT SHIFT, H, split-changemonitor, prev
